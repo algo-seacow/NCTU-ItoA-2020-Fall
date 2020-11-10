f880d64d05b24dd60982a68cbe3ab7c4802d92ec
Tatsiujin Chin <c910335@gmail.com>
2020-11-10 10:53:31 +0800

    fix 1119

--- 1119/README.md
+++ 1119/README.md
@@ -4,9 +4,13 @@
 
 - 自己實作 Hash Table
   - [0756127_hash_table_list.c](submissions/accepted/0756127_hash_table_list.c)
+- Sort + Binary Search
+  - [309552025_sort.cpp](submissions/accepted/309552025_sort.cpp)
+
+注意以下實作在相同的和重複出現的情況效率會比較差
+
+- 自己實作 Hash Table
   - [309552025_hash_table.cpp](submissions/accepted/309552025_hash_table.cpp)
 - unordered_multimap
   - [0756127_unordered_multimap.cpp](submissions/accepted/0756127_unordered_multimap.cpp)
   - [309552025_unordered_multimap.cpp](submissions/accepted/309552025_unordered_multimap.cpp)
-- Sort + Binary Search
-  - [309552025_sort.cpp](submissions/accepted/309552025_sort.cpp)
--- 1119/submissions/accepted/0756127_hash_table_list.c
+++ 1119/submissions/accepted/0756127_hash_table_list.c
@@ -12,23 +12,34 @@ typedef struct E {
 int n, s, i, a[SIZE], j, q = -1, w, e, r;
 E *ht[HT_SIZE], *t;
 
-void add() {
-  t = malloc(sizeof(E));
-  t->i = i;
-  t->j = j;
-  t->s = a[i] + a[j];
-  t->n = ht[h(t->s)];
-  ht[h(t->s)] = t;
-}
-
 int main() {
   scanf("%d%d", &n, &s);
   for (; i != n; ++i)
     scanf("%d", a + i);
 
   for (j = 1; j < n - 2; ++j) {
-    for (i = 0; i != j; ++i)
-      add();
+    for (i = 0; i != j; ++i) {
+      t = ht[h(s - a[i] - a[j])];
+      while (t) {
+        if (t->s == s - a[i] - a[j])
+          break;
+        t = t->n;
+      }
+
+      if (t) {
+        if (i < t->i || i == t->i && j < t->j) {
+          t->i = i;
+          t->j = j;
+        }
+      } else {
+        t = malloc(sizeof(E));
+        t->i = i;
+        t->j = j;
+        t->s = a[i] + a[j];
+        t->n = ht[h(t->s)];
+        ht[h(t->s)] = t;
+      }
+    }
     for (i = j + 2; i != n; ++i) {
       t = ht[h(s - a[j + 1] - a[i])];
       while (t) {
