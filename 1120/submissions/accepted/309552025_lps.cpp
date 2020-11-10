3061c034f7ad95e8f1686dd18fcfce0ff889068b
yesa1791 <yesa1791@gmail.com>
2020-11-11 00:25:34 +0800

    add 1120

---
+++ 1120/submissions/accepted/309552025_lps.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	string str;
+	cin >> n >> str;
+	vector<vector<int>> dp(n + 2, vector<int>(n + 2));
+	for(int i = n - 1; i >= 0; i--){
+		dp[i][i] = 1;
+		dp[i][i+1] = max(dp[i][i], dp[i+1][i+1]);
+		for(int j = i + 2; j < n; j++){
+			if(str[i] == str[j]){
+				dp[i][j] = dp[i+2][j-2] + 2;
+			}
+			else{
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+			}
+		}
+	}
+	cout << dp[0][n-1] << '\n';
+}
