34e82ebfa47625641f555e6903d7ac1abb0d4a10
yesa1791 <yesa1791@gmail.com>
2020-11-11 00:25:34 +0800

    add 1120

--- 1120/README.md
+++ 1120/README.md
@@ -13,6 +13,7 @@
     - 最後答案是 dp[0][n - 1]
 
 - [0756127_lps.c](submissions/accepted/0756127_lps.c)
+- [309552025_lps.cpp](submissions/accepted/309552025_lps.cpp)
 
 2. 和倒過來的 s（以下稱其為 r）做 LCS，修改遞迴式讓它不能用相鄰的字元
     - dp[i][j]: s 前 i 個字元和 r 前 j 個字元不用相鄰字元的 LCS 長度
@@ -25,3 +26,4 @@
     - 最後答案是 dp[n - 1][n - 1]
 
 - [0756127_reversed_lcs.cpp](submissions/accepted/0756127_reversed_lcs.cpp)
+- [309552025_reversed_lcs.cpp](submissions/accepted/309552025_reversed_lcs.cpp)
---
+++ 1120/submissions/accepted/309552025_reversed_lcs.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	string str;
+	cin >> n >> str;
+	string rev(str.rbegin(), str.rend());
+	vector<vector<int>> dp(n, vector<int>(n));
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(str[i] == rev[j]){
+				dp[i][j] = ((i > 1 && j > 1)? dp[i-2][j-2] : 0) + 1;
+			}
+			else{
+				int l = j > 0 ? dp[i][j-1] : 0;
+				int u = i > 0 ? dp[i-1][j] : 0;
+				dp[i][j] = max(l, u);
+			}
+		}
+	}
+	cout << dp[n-1][n-1] << '\n';
+}
